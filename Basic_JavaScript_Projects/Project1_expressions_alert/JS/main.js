de6495680db2dcfb3452a9b01125a39048994864
Kriszha Real <yujmin901205@gmail.com>
2024-08-24 05:37:11 +0800

    First JS Project Push
    utilized javascript methods, added string values and used concatenation, assigned colors to variables, wrote an expression, added comments

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+var A = "This is a string";
+window.alert(A);
+
+document.write("\"Be who you are and say what you feel," + " because those who mind don\'t matter and those who matter don\'t mind.\"" + "- Dr. Seuss <br>");
+
+var B = "This is a " + "string!";
+document.write(B);
+
+var Fam = "The Ahns", Dada = "Yujin", Mommy = "Minju", Daughter = "Leeseo"; //initializing variables
+var Dada = Dada.fontcolor("blue"); //turning dada's color blue
+Mommy = Mommy.fontcolor("gray"); 
+Daughter = Daughter.fontcolor("skyblue");
+document.write("<br>" + Dada + " " + Mommy + " " + Daughter);
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write("<br>" + blues);
+
+document.write("<br>");
+document.write(3+3);
+
